98327a118e26ac89cdcaec6308402ab6c303a6f3
Xavier Gouley <xavier.gouley@prestashop.com>
2017-06-22 14:53:32 +0200

    basic tests on component OK

--- test/lib/index-test.js
+++ test/lib/index-test.js
@@ -8,6 +8,7 @@ import { expect } from 'chai'
 import { mount } from 'enzyme'
 
 import Gridifier from '../../lib/index'
+import ExplicitSaving from '../../lib/order-handler/explicit-saving'
 
 describe('[Gridifier] React component', () => {
   it('can be mounted.', () => {
@@ -27,7 +28,44 @@ describe('[Gridifier] React component', () => {
     expect(wrapper.hasClass('test-abc')).to.equal(true)
   })
 
-  it('accepts JqGridifier settings as properties.')
+  it('accepts JqGridifier settings as properties.', () => {
+    const options = {
+      class: 'test',
+      grid: 'vertical',
+      prepend: 'mirrored',
+      append: 'default',
+      intersections: true,
+      align: 'top',
+      sortDispersion: true,
+      loadImages: true,
+      toggleTime: 500,
+      coordsChangeTime: 300,
+      toggleTiming: 'ease',
+      coordsChangeTiming: 'ease',
+      rotatePerspective: '100px',
+      rotateBackface: false,
+      rotateAngles: [0, -180, 180, 0],
+      gridResize: 'fit',
+      gridResizeDelay: 100,
+      dragifier: 'rg-drag-handler',
+      dragifierMode: 'i',
+      widthPxAs: 0,
+      heightPxAs: 0,
+      queueSize: 12,
+      queueDelay: 25
+    }
+    const wrapper = mount(<Gridifier {...options} />)
+    expect(wrapper.hasClass('rg-grid')).to.equal(true)
+  })
 
-  it('accepts structuring properties (editable, orderHandler, insertionMode, itemBackgrounds).')
+  it('accepts structuring properties (editable, orderHandler, insertionMode, itemBackgrounds).', () => {
+    const options = {
+      editable: true,
+      orderHandler: new ExplicitSaving(),
+      insertionMode: 'prepend',
+      itemBackgrounds: true
+    }
+    const wrapper = mount(<Gridifier {...options} />)
+    expect(wrapper.hasClass('rg-grid')).to.equal(true)
+  })
 })
